23f2e34c80f04ad1fff0f28c40a2654c78765c37
Marta <martadabrowka@gmail.com>
2018-08-26 10:06:03 +0100

    adds get route for questions

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var bodyParser = require('body-parser');
+var AWS = require("aws-sdk");
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.listen(3000, () => console.log('Questions API listening on port 3000!'))
+AWS.config.update({
+  region: "eu-west-2",
+  endpoint: "http://localhost:8000"
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.set('view engine', 'jade');
+app.get('/', function (req, res) {
+  res.send({ title: "Questions API Entry Point" })
+})
+app.get('/questions', function (req, res) {
+var params = {
+    TableName: "Questions",
+    ProjectionExpression: "#question",
+    ExpressionAttributeNames: {
+        "#question": "question",
+    }
+};
+console.log("Scanning Questions table.");
+docClient.scan(params, onScan);
+function onScan(err, data) {
+    if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        res.send(data)
+        // print all the Questions
+        console.log("Scan succeeded.");
+        data.Items.forEach(function(question) {
+           console.log(question.question)
+        });
+if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            docClient.scan(params, onScan);
+        }
+    }
+  }
+})
 
 module.exports = app;
