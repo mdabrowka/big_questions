886f03338b75e0736fa34445b07d44218bacd80d
Marta <martadabrowka@gmail.com>
2018-08-26 11:37:50 +0100

    adds home page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AWS = require("aws-sdk");
 var app = express();
-app.listen(3000, () => console.log('Questions API listening on port 3000!'))
+app.listen(3000, () => console.log('Big Questions of our time listening on port 3000!'))
 AWS.config.update({
   region: "eu-west-2",
   endpoint: "http://localhost:8000"
@@ -13,11 +13,15 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'jade');
-app.get('/', function (req, res) {
-  res.send({ title: "Questions API Entry Point" })
-})
-app.get('/questions', function (req, res) {
+//app.set('view engine', 'jade');
+// app.get('/', function (req, res) {
+//   res.send({ title: "Welcome to the Big Questions of Our Time" })
+// })
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/api/get-questions', function (req, res) {
 var params = {
     TableName: "Questions",
     ProjectionExpression: "#question",
@@ -44,6 +48,23 @@ if (typeof data.LastEvaluatedKey != "undefined") {
         }
     }
   }
+})
+
+var emails = ['marta'];
+
+app.post('/api/subscribe', function(req, res) {
+  console.log(req.body);
+  var subscriber = req.body;
+  emails.push(subscriber);
+    var params = {
+            TableName: "Subscribers",
+            Item: {
+                "id": emails.length + 1,
+                "name": subscriber.name,
+                "email": subscriber.email
+            }
+        };
+        console.log('logging params', params);
 })
 
 module.exports = app;
